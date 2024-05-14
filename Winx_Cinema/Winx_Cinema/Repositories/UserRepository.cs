0303bc99733e7e3454887fc5ccf4bfabb99cf3d4
Bohdan <galanbogdan33@gmail.com>
2024-05-14 23:49:21 +0300

    add changes to user repository

--- Winx_Cinema/Winx_Cinema/Repositories/UserRepository.cs
+++ Winx_Cinema/Winx_Cinema/Repositories/UserRepository.cs
@@ -1,6 +1,15 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.CodeDom.Compiler;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Winx_Cinema.Data;
 using Winx_Cinema.Interfaces;
+using Winx_Cinema.Shared.Dtos;
 using Winx_Cinema.Shared.Entities;
 
 namespace Winx_Cinema.Repositories
@@ -8,19 +17,91 @@ namespace Winx_Cinema.Repositories
     public class UserRepository : IUserRepository
     {
         private readonly AppDbContext _context;
-        public UserRepository(AppDbContext context)
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<User> userManager;
+        private readonly IMapper mapper;
+        public UserRepository(AppDbContext context, IConfiguration configuration, UserManager<User> userManager, IMapper mapper)
         {
             _context = context;
+            _configuration = configuration;
+            this.userManager = userManager;
+            this.mapper = mapper;
+        }
+
+        public async Task<LoginResponseDto> Login(LoginDto userDto)
+        {
+            var user = await userManager.FindByEmailAsync(userDto.Email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            var result = await userManager.CheckPasswordAsync(user, userDto.Password);
+
+            if (!result)
+            {
+                return null;
+            }
+
+            var token = GenerateJwtToken(user);
+
+            return new LoginResponseDto
+            {
+                user = user,
+                Token = token,
+            };
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512),
+
+            };
+
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
 
         public async Task<ICollection<User>> GetAll() => await _context.Users.ToListAsync();
 
         public async Task<User?> Get(string id) => await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
 
-        public async Task Add(User user)
+        public async Task CreateUser(NewUserDto model)
         {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+
+            var user = await userManager.FindByEmailAsync(model.Email);
+
+            if (user != null)
+            {
+                return;
+            }
+
+            user = new User()
+            {
+                Email = model.Email,
+                FirstName = model.FirstName,
+                SecondName = model.SecondName,
+                UserName = model.UserName,
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return;
+            }
+            return;
         }
 
         public async Task<bool> Update(User user)
